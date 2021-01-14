c4696764cb6d846b6bba87fcea0188d8d405aaab
Kim Van de Casteele <kim.vandecasteele@sfpd.fgov.be>
2021-01-14 12:51:42 +0100

    Chapter 1 Introduction

---
+++ Introduction Java Programming/com/company/Chapter01Introduction/ProgEx/ShowSyntaxErrors.java
@@ -0,0 +1,11 @@
+package com.company.Chapter01Introduction.ProgEx;
+
+public class ShowSyntaxErrors {
+    // Errors that are detected by the compiler are called syntax errors or compile errors.
+
+    // The keyword void is missing
+    // The string Welcome to Java should be closed with a closing quotation mark
+//    public static main (String [] args) {
+//        System.out.println("Welcome to Java);
+//    }
+}
--- Introduction Java Programming/com/company/JavaCourse/Course/day3/Vacuumcleaner.java
+++ Introduction Java Programming/com/company/JavaCourse/Course/day3/Vacuumcleaner.java
@@ -1,7 +1,5 @@
 package com.company.JavaCourse.Course.day3;
 
-import com.company.JavaCourse.Course.day3.Address;
-
 public class Vacuumcleaner {
     public void vacuum(Address address){
         System.out.println("vacuum noise, to clean the house at " + address.getCity() + " " + address.getStreetName() + " " + address.getNr());
