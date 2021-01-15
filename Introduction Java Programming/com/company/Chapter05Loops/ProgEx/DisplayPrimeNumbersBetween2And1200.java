7e645defbcb50af3a0d5e997b6c672f40b557ef4
Kim Van de Casteele <Kinkyk013@gmail.com>
2021-01-15 19:31:49 +0100

    Prog Ex Chapter 5 => 5.23

---
+++ Introduction Java Programming/com/company/Chapter05Loops/ProgEx/DisplayPrimeNumbersBetween2And1200.java
@@ -0,0 +1,8 @@
+package com.company.Chapter05Loops.ProgEx;
+
+public class DisplayPrimeNumbersBetween2And1200 {
+    /*  Modify PrimeNumber.java to display all the prime numbers between 2 and 1200 inclusive. Display eight prime
+        numbers per line. Numbers are separated by exactly one space.
+     */
+
+}
