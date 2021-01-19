3ff923e0ca2fe55369b4026b6192272bdcc053c2
Kim Van de Casteele <Kinkyk013@gmail.com>
2021-01-19 12:37:51 +0100

    Setup Prog Ex Chapter 5 finished Done: exercises 1 tem 5

--- Introduction Java Programming/com/company/Chapter05Loops/ProgEx/ConversionFromInchToCm.java
+++ Introduction Java Programming/com/company/Chapter05Loops/ProgEx/ConversionFromInchToCm.java
@@ -9,5 +9,15 @@ public class ConversionFromInchToCm {
             9                       22.86
             10                      25.4
      */
-
+    public static void main(String[] args) {
+        double inches = 1.0;
+        double centimeters = 0.0;
+        System.out.println("Inches          Centimeters");
+        while (inches <= 10){
+            centimeters = inches * 2.54;
+            System.out.println(Math.round(inches) + "               " + centimeters);
+            ++inches;
+            centimeters = inches * 2.54;
+        }
+    }
 }
---
+++ Introduction Java Programming/com/company/Chapter05Loops/ProgEx/HeadsOrTails.java
@@ -0,0 +1,6 @@
+package com.company.Chapter05Loops.ProgEx;
+
+public class HeadsOrTails {
+    //  Write a program that simulates flipping a coin two millions times and displays the number of heads or tails.
+
+}
