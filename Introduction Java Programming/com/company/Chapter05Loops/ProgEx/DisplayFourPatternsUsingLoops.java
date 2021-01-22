678256d2b9841d798b31557a6dec75a475d8fcb3
Kim Van de Casteele <Kinkyk013@gmail.com>
2021-01-22 08:59:06 +0100

    Prog ex Chapter 5: 5.11 tem 5.18

--- Introduction Java Programming/com/company/Chapter05Loops/ProgEx/DisplayFourPatternsUsingLoops.java
+++ Introduction Java Programming/com/company/Chapter05Loops/ProgEx/DisplayFourPatternsUsingLoops.java
@@ -1,5 +1,7 @@
 package com.company.Chapter05Loops.ProgEx;
 
+import java.util.Arrays;
+
 public class DisplayFourPatternsUsingLoops {
     // Use nested loops that display the following patterns in four separate programs:
     /*  Pattern A       Pattern B       Pattern C       Pattern D
@@ -10,5 +12,42 @@ public class DisplayFourPatternsUsingLoops {
         * * * * *       * *             * * * * *               * *
         * * * * * *     *             * * * * * *                 *
      */
-    
+    public static void main(String[] args) {
+        System.out.println("\n\nPattern D");
+        for (int i = 6; i <= 6; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print('*' + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n\nPattern C");
+        for (int i = 1; i <= 6; i++)    {
+            for (int k = 1; k <= 6 - i; k++){
+                System.out.print("  ");
+            }
+            for (int j = 1; j <=  i; j++) {
+                System.out.print('*' + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n\nPattern A");
+        for (int i = 1; i <= 6; i++) {
+            for(int j = 1 ; j <= i; j++){
+                System.out.print('*' + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n\nPattern B");
+        for (int i = 6; i <= 6; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print('*' + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
 }
--- Introduction Java Programming/com/company/Chapter05Loops/ProgEx/PassOrFail.java
+++ Introduction Java Programming/com/company/Chapter05Loops/ProgEx/PassOrFail.java
@@ -1,5 +1,7 @@
 package com.company.Chapter05Loops.ProgEx;
 
+import java.util.Scanner;
+
 public class PassOrFail {
     /*  Write a program that prompts a student to enter a Java score. If the score is greater or equal to 60, display
         "you pass the exam"; otherwise, display "you don't pass the exam". Your program ends with input -1.
@@ -13,5 +15,21 @@ public class PassOrFail {
             Enter your score: -1
             No numbers are entered except 0.
      */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your score: ");
+        int score = input.nextInt();
+        String result = " ";
 
+        while(result == " "){
+            if (score >= 60){
+              result = "you pass the exam.";
+          } else if (score < 60 && score > 0) {
+              result = "you don't pass the exam.";
+          } else if (score < 0){
+              result = "no numbers are entered except 0.";
+          }
+        }
+        System.out.println(result);
+    }
 }
