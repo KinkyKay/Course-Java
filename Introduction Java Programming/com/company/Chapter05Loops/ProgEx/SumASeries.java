8c2c096742a5c34546234e811875400f284e8c17
Kim Van de Casteele <Kinkyk013@gmail.com>
2021-01-24 17:27:46 +0100

    Prog ex Chapter 5: 5.18 tem 5.24

--- Introduction Java Programming/com/company/Chapter05Loops/ProgEx/SumASeries.java
+++ Introduction Java Programming/com/company/Chapter05Loops/ProgEx/SumASeries.java
@@ -4,5 +4,7 @@ public class SumASeries {
     /*  Write a program to compute the following summation:
         1/3 + 3/5 + 5/7 + 9/11 + 11/13 + ... + 95/97 + 97/99
      */
+    public static void main(String[] args) {
 
+    }
 }
